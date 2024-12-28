ece7f38177830d018b7e3eb887eccb00076b9cbf
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2024-12-28 18:45:27 +0100

    Update ExtractHash.ps1

--- AD/ExtractHash.ps1
+++ AD/ExtractHash.ps1
@@ -1,5 +1,5 @@
 #Show and save all process hash
-$Paths = Get-Process | Select-Object -ExpandProperty Path | Sort-Object -Unique | Where-Object { $_ -ne $null }
+$Paths = Get-Process | Select-Object -ExpandProperty Path | Sort-Object -Unique
 foreach($path in $paths){
     $hs = $(Get-FileHash -Algorithm SHA256 $path).Hash
     $filename = Split-Path -Path $path -Leaf 
