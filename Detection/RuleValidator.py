ed07fd6d32ff76416b70046791929b2ee0d7cb49
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2025-03-22 23:34:02 +0100

    Update RuleValidator.py

--- Detection/RuleValidator.py
+++ Detection/RuleValidator.py
@@ -1,14 +1,15 @@
 import tomllib
 import sys 
+import glob
+import os
 
-def check_valid(file):
+def check_valid(file_content):  
     present_fields = []
     required_fields = ["description", "name", "risk_score", "severity", "type", "query"]  
     missing_fields = []
 
-    # Print the dictionary directly
-    for table in file:
-        for field in file[table]:
+    for table in file_content:
+        for field in file_content[table]:
             present_fields.append(field)
 
     for field in required_fields:
@@ -20,16 +21,42 @@ def check_valid(file):
     else:
         print("All good")
 
-
-if sys.argv[1:] == []:
-    print("Usage : python3 valid.py <filepath>")
-else:
-    file_path = sys.argv[1]
+def check_file(file_path):
+    """Check a single TOML file."""
+    if not os.path.isfile(file_path):
+        print(f"The file {file_path} does not exist.")
+        return
+    
     with open(file_path, "rb") as rule:
         try:
-            file = tomllib.load(rule)  
-            check_valid(file)
+            file_content = tomllib.load(rule)
+            print(f"\nChecking file: {file_path}")
+            check_valid(file_content)
         except tomllib.TOMLDecodeError as e:
-            print(e)
+            print(f"Error decoding {file_path}: {e}")
+
+def check_folder(file_path):
+    """Check all TOML files in a folder."""
+    folder = os.path.join(file_path, "*.toml")  
+    toml_files = glob.glob(folder)
     
-    
+    if not toml_files:
+        print("No .toml files found.")
+        return
+
+    for toml in toml_files:
+        check_file(toml)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python3 valid.py <filepath> <rule or folderrules>")
+    else:
+        file_path = sys.argv[1]
+        option = sys.argv[2]
+
+        if option == "rule":
+            check_file(file_path)
+        elif option == "folderrules":
+            check_folder(file_path)
+        else:
+            print("Invalid option. Use 'rule' for a single file or 'folderrules' for a folder.")
